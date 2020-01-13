87b504d5e71d1dfff02e36327c51d80c9b1e4016
John <956407921@qq.com>
2020-01-14 00:51:41 +0800

    searchJS complete

--- src/js/index/page-3.js
+++ src/js/index/page-3.js
@@ -9,41 +9,47 @@
 		},
 		template: `
 			<form><input type="text" autofocus placeholder="搜索歌曲">
-				<svg class="icon clear" aria-hidden="true">
+				<svg class="icon clear bind" aria-hidden="true">
 				    <use xlink:href="#icon-X"></use>
 				</svg>
 			</form>
-			<p class="prompt">123</p>
+			<p class="prompt bind">123</p>
 		`,
 		render(data={}){
 			if(JSON.stringify(data) === '{}'){
 				$(this.el).find('.search').html(this.template)
 			}
 			else{
-				let {songs} = data
-				let $ali = songs.map((song)=>{	
-					return $(`
-					<a href='./song.html?id=${song.id}'>
-						<div class="songName">${song.name}</div>
-						<div class="songSinger">
+				$(this.el).find('.songs').empty()
+				let {songs,search} = data
+				if(search === '0'){
+					$(this.el).find('.songs').html('<p>暂无搜索结果</p>')
+				}else{
+					let $ali = songs.map((song)=>{
+						return $(`
+						<a href='./song.html?id=${song.id}'>
+							<div class="songName">${song.name}</div>
+							<div class="songSinger">
+								<svg class="icon" aria-hidden="true">
+									<use xlink:href="#icon-sq3"></use>
+								</svg>
+								${song.singer}
+							</div>
 							<svg class="icon" aria-hidden="true">
-								<use xlink:href="#icon-sq3"></use>
+								<use xlink:href="#icon-bofang"></use>
 							</svg>
-							${song.singer}
-						</div>
-						<svg class="icon" aria-hidden="true">
-							<use xlink:href="#icon-bofang"></use>
-						</svg>
-					</a>
-					`)
-				})
-				$(this.el).find('.songs').append($ali)
+						</a>
+						`)
+					})
+					$(this.el).find('.songs').append($ali)
+				}
 			}
 		}
 	}
 	let model = {
 		data:{
-			songs:[]
+			songs:[],
+			search:'' //标志搜索结果 '0' 没有 '1' 有
 		},
 		find(queryParas){
 			var query1 = new AV.Query('Song');
@@ -53,6 +59,11 @@
 			var query = AV.Query.or(query1, query2);
 			
 			return query.find().then((songs)=>{
+				if(songs.length===0){
+					this.data.search = '0'
+					}else{
+						this.data.search = '1'
+					}
 				this.data.songs = songs.map((song)=>{
 					return {id: song.id, ...song.attributes}
 				})
@@ -80,18 +91,37 @@
 		bindEvents(){
 			$(this.view.el).on('input','input',(e)=>{
 				// 实时显示并搜索	
+				$(this.view.el).find('.songs').empty()
 				let val = $(e.currentTarget).val()
-				$(this.view.el).find('.prompt').text('搜索: '+ val )
+				if(val){
+					$(this.view.el).find('.prompt').text('搜索 '+ '"' + val + '"')
+					this.view.show($('.bind'))
+				}else{
+					this.view.hide($('.bind'))
+				}
 			})
+			
 			$(this.view.el).on('submit','form',(e)=>{
 				//按回车的时候 执行
 				e.preventDefault()
+				this.view.hide($('.prompt'))
 				let value = $(this.view.el).find('input').val()
 				this.model.find(value).then(()=>{
-					console.log(this.model.data)
 					this.view.render(this.model.data)
 				})
-				
+			})
+			
+			$(this.view.el).on('click','.clear',()=>{ //清空并隐藏
+				$(this.view.el).find('input').val('')
+				this.view.hide($('.bind'))
+			})
+			
+			$(this.view.el).on('click','.prompt',()=>{ //清空并隐藏
+				this.view.hide($('.prompt'))
+				let value = $(this.view.el).find('input').val()
+				this.model.find(value).then(()=>{
+					this.view.render(this.model.data)
+				})
 			})
 		}
 	}
